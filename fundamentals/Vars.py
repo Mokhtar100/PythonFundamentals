39d66c038b50796d6a5fde92c54abc00cbcb65df
mokhtar <mohamedmokhtarm3@gmail.com>
2024-01-13 03:32:12 +0200

    variables

--- fundamentals/Vars.py
+++ fundamentals/Vars.py
@@ -1,1 +1,55 @@
-x=1
+# variables:is a name that refers a value
+# variable" is a container used to store data
+# variables are case sensitive
+# Example of declaring and using variables
+var = 10
+var = "Hello, World!"
+
+print(var)
+print(var)
+
+# conditions to declare variables:
+    # 1. variable names cannot start with numbers
+    # 2. variable names cannot start with special characters
+    # 3. variable names cannot contain spaces
+    # 4. variable names should be meaningful
+    # 5. variable names cannot use reserved words as variable names 
+            # list of reserved words (23): 
+                #false - as - continue - else - from - in - not- return -
+                # none - assert - def - except - global - is - or - try - true                         # break - del - finally - if - lambda - pass - while - and - class-
+                # elif - for - import - nonlocal -raise - with 
+
+# Assigning values to variables:    
+    # 1-single assignment:
+myVariable = 10
+    # 2-multiple assignment:
+x,y,z=1,2,3
+    
+#variables type:
+    # 1. int       - whole numbers
+    # 2. float     - decimal numbers
+    # 3. string    - text
+    # 4. boolean   - true or false
+# Integer variable
+my_int = 10
+
+# Float variable
+my_float = 3.14
+
+# String variable
+my_string = "Hello, World!"
+
+# Printing the variables
+print(my_int)
+print(my_float)
+print(my_string)
+
+# boolean variable
+my_bool = True
+
+# function type(my_variable) to check the type of variable 
+print(type(my_bool))
+
+# use semicolon to terminate statements:
+
+my_int = 10; my_float = 3.14; my_string = "Hello, World!"
