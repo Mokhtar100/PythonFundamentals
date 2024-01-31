830ff1f823149c026c5c30bb5ce4f62c9f7756b7
mokhtar <mohamedmokhtarm3@gmail.com>
2024-01-31 05:02:30 +0200

    tuples

---
+++ fundamentals/Tuples.py
@@ -0,0 +1,24 @@
+# Tuples in Python
+# Tuples are used to store multiple items in a single variable
+# Tuples are ordered, unchangeable, and allow duplicate values
+# Tuples are enclosed in parentheses ()
+# Tuples are immutable
+# Tuples are iterable
+# Tuples are sequences of values
+# You can convert a tuple to a list and back to a tuple
+# example:
+my_tuple = (1, 2, 3, 4, 5)
+print(my_tuple)
+
+# built-in functions:
+# 1. len() # returns the length of the tuple
+# 2. count() # returns the number of occurrences of the specified value
+# 3. index() # returns the index of the first occurrence of the specified value
+# 4. copy() # returns a copy of the tuple
+# 5. max() # returns the largest item in the tuple
+# 6. min() # returns the smallest item in the tuple
+
+#*** you can convert a tuple to a list and back to a tuple:
+my_tuple = (1, 2, 3, 4, 5)
+my_list = list(my_tuple)
+
